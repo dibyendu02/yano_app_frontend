0d3afd3213168da39139b1e2b2cab45eebba1b26
Joker-40 <samuip8@gmail.com>
2024-07-27 15:16:28 +0530

    Refactoring

---
+++ src/components/cards/CardStyle.ts
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from '../../constants/Colors';
+
+export const CardStyles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginVertical: 6,
+    borderRadius: 10,
+    backgroundColor: Colors.White,
+    width: '94%',
+    alignSelf: 'center',
+    overflow: 'hidden',
+  },
+  content: {
+    padding: 15,
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Roboto',
+    fontWeight: '600',
+    fontSize: 12,
+    color: Colors.SteelBlue,
+    alignSelf: 'flex-start',
+  },
+});
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,15 +2,23 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import Icons from '../../assets/icon/Icon';
 import {useNavigation} from '@react-navigation/native';
+import {Colors} from '../../constants/Colors';
 
-const Header = () => {
+interface HeaderProps {
+  title?: string;
+  showBackIcon?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({title, showBackIcon = true}) => {
   const navigation = useNavigation();
   return (
     <View style={styles.navbar}>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Icons.AntDesign name="arrowleft" size={28} color={'black'} />
-      </TouchableOpacity>
-      <Text style={styles.navbarTitle}>Monitored Patient</Text>
+      {showBackIcon && (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icons.AntDesign name="arrowleft" size={28} color={'black'} />
+        </TouchableOpacity>
+      )}
+      <Text style={styles.navbarTitle}>{title}</Text>
     </View>
   );
 };
@@ -23,6 +31,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 20,
     paddingHorizontal: 10,
+    backgroundColor: Colors.White,
   },
   navbarTitle: {
     color: 'black',
